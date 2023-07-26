cb8654052aa9fbc243422675fb01ab1648abca1a
Mary <mmebit@icloud.com>
2023-07-25 23:16:03 -0400

    counted the magic

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  return <h1>Hello, world!</h1>
+  const name = "Mary";
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>THIS IS ACTUALLY {count} MAGIC</h1>
+      <button id="thisButton" onClick={(e)=>{setCount(count+1)}}>Seent</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
