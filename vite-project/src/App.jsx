7d036d787aa26db16a113b7a0fc3cf0e167097c9
mmelines <64270-mmelines@users.noreply.drupalcode.org>
2023-07-26 23:01:33 -0400

    boom! that is the power of react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,25 +7,55 @@ function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
 
-  function handleNameChange (e) {
+  function handleNameChange(e) {
     setName(e.target.value);
-  };
+  }
 
   return (
     <div>
-      <h1>THIS IS ACTUALLY {count} MAGIC</h1>
-      <div>They call me {name}</div>
+      <h1>ToDo List</h1>
+      {/* <div>They call me {name}</div>
       <div children={<h2>Goodbye, moonman</h2>}/>
       <input id="FINE" type="text" value={name} onChange={handleNameChange} />
-      <button id="thisButton" onClick={(e)=>{setCount(count+1)}}>Seent</button>
+      <button id="thisButton" onClick={(e)=>{setCount(count+1)}}>Seent</button> */}
 
-      <ToDos title={name}/>
+      <ToDos title="To Do List:" />
     </div>
   );
 }
 
 function ToDos(props) {
-  return <div><h3 style={{padding: 30, backgroundColor: 'pink'}}>{props.title}</h3></div>
+  const [todos, setTodos] = useState([
+    { name: "take out the garbage", id: 1 },
+    { name: "build a tree", id: 2 },
+    { name: "plant a house", id: 3 },
+    { name: "research recipes", id: 4 },
+  ]);
+  const [newToDo, setNewToDo] = useState("");
+  const appendToDos = function (e) {
+    let updatedArray = [...todos,{name:newToDo, id:(todos.length+1)}];
+    setTodos(updatedArray);
+    setNewToDo("");
+  };
+  return (
+    <div>
+      <div id="input-container">
+        <label htmlFor="newToDoInput">What are you doin' today?</label>
+        <input id="newToDoInput" type="text" value={newToDo} onChange={(e)=>{setNewToDo(e.target.value)}}></input>
+        <div id="displayNewTodoInput">{newToDo}</div>
+        <button id="sureAboutToDoButton" onClick={appendToDos}>Hit it!</button>
+      </div>
+      <div id="list-container">
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id} id={`todo-${todo.id}`}>
+              {todo.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
